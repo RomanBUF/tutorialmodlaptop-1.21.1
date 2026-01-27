8180b8d45b80324e173f5e0ed3dcf29eff2dbfaa
Roma <romaroma.b.2009@gmail.com>
2026-01-27 20:04:33 +0700

    new

--- src/main/java/net/zero/tutorialmod/TutorialModClient.java
+++ src/main/java/net/zero/tutorialmod/TutorialModClient.java
@@ -1,8 +1,15 @@
 package net.zero.tutorialmod;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.block.BlockState;
+import net.zero.tutorialmod.block.custom.LandMineBlock;
 
 public class TutorialModClient implements ClientModInitializer {
+
     @Override
     public void onInitializeClient() {
 
